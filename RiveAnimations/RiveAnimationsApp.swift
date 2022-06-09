2958b3deb4564489d6e9aaaa72a0dc573229e9ba
andriimuzh <muzhandriy@gmail.com>
2022-06-09 13:59:09 +0300

    Initial Commit

---
+++ RiveAnimations/RiveAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationsApp.swift
+//  RiveAnimations
+//
+//  Created by Andrii Muzh on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
