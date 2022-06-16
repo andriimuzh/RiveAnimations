185ab74bd2a48b8cb126baed0859fa07073cc674
andriimuzh <muzhandriy@gmail.com>
2022-06-16 14:55:49 +0300

    side bar menu animation

--- RiveAnimations/ContentView.swift
+++ RiveAnimations/ContentView.swift
@@ -15,6 +15,14 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            LinearGradient(
+                colors: [Color(hex: "#17203A"), Color(hex: "#394156")], startPoint: .top, endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            SideMenu()
+                .opacity(isOpen ? 1 : 0)
+                .offset(x: isOpen ? 0 : -300)
+            
             Group {
                 switch selectedTab {
                 case .chat:
@@ -35,18 +43,28 @@ struct ContentView: View {
             .safeAreaInset(edge: .top) {
                 Color.clear.frame(height: 104)
             }
+            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .rotation3DEffect(.degrees(isOpen ? 30 : 0), axis: (x: 0, y: -1, z: 0))
+            .offset(x: isOpen ? 250 : 0)
+            .scaleEffect(isOpen ? 0.9 : 1)
             .ignoresSafeArea()
+            
             button.view()
                 .frame(width: 44, height: 44)
                 .mask(Circle())
                 .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .padding()
+                .offset(x: isOpen ? 216 : 0)
                 .onTapGesture {
                     button.setInput("isOpen", value: isOpen)
-                    isOpen.toggle()
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7)) {
+                        isOpen.toggle()
+                    }
                 }
             BottomTabBar()
+                .offset(y: isOpen ? 150 : 0)
+                .opacity(isOpen ? 0 : 1)
         }
     }
 }
